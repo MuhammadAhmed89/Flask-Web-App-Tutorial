3636c461b890ec68126ab53aca103c1c405e0189
K1934685 <k1934685@kingston.ac.uk>
2022-07-19 23:52:42 +0100

    fyp
    fyp

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from website import create_app
-
+import os
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
 app = create_app()
 
 if __name__ == '__main__':
+    #path
+    path='images'
+    images=os.listdir(path)
+    type(images)
+    print(len(images))
+    img_data=[]
+    for img in images:
+        img_arr=cv2.imread(os.path.join(path,img))
+        img_data.append(img_arr)
+        
+    plt.figure(figsize=(10,10))
+    for i in range(len(img_data)):
+        plt.subplot(6,6,i+1)
+        plt.imshow(img_data[i])
+
+
+    file = "C:/Users/Desktop/Documents/FYP/images/img1.jpg"
+    image = Image.open(file)
+    image.show()
     app.run(debug=True)
+
